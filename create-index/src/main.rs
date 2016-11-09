6993831d8e4cbe133b42193667b2e620f8c9547d
Ysmiraak <ysmiraak@gmail.com>
2016-11-09 17:09:54 +0100

    fixed some problems

--- create-index/src/main.rs
+++ create-index/src/main.rs
@@ -9,11 +9,11 @@ use std::collections::HashMap;
 use std::hash::Hash;
 
 trait SortedSet<T:Ord+Copy> {
-    fn grow(self, item:T) -> Self;
+    fn inc(self, item:T) -> Self;
 }
 
 impl<T> SortedSet<T> for Vec<T> where T:Ord+Copy {
-    fn grow(mut self, item:T) -> Self {
+    fn inc(mut self, item:T) -> Self {
         match self.binary_search(&item) {
             Ok(_) => (),
             Err(idx) => self.insert(idx, item)
@@ -22,10 +22,10 @@ impl<T> SortedSet<T> for Vec<T> where T:Ord+Copy {
     }
 }
 
-fn update<K,V,X>(mut m:HashMap<K,V>, k:K, f:&Fn(Option<V>,X) -> V, x:X) -> HashMap<K,V>
+fn update<K,V>(mut m:HashMap<K,V>, k:K, f:&Fn(Option<V>) -> V) -> HashMap<K,V>
     where K:Hash+Eq {
     let v = m.remove(&k);
-    m.insert(k,f(v,x));
+    m.insert(k,f(v));
     m
 }
 
@@ -38,21 +38,29 @@ fn main() {
     //     _ => println!("usage: create-index CONLLX_PATH INDEX_PATH"),
     // }
 
-    let conllx_in_file_name = "tubadw-r1-ir-sample-1000";
+    let conllx_in_file_name = "tubadw-r1-ir-sample-100000";
     let index_out_file_name = "index.txt";
 
     let conllx_in = File::open(conllx_in_file_name).unwrap();
     let index_out = File::create(index_out_file_name).unwrap();
 
-    let ref grow_posting = |s:Option<Vec<usize>>, i:usize| match s {
-        Some(s) => s.grow(i), None => Vec::new() };
+    let ref grow_posting = |mv:Option<Vec<usize>>, i:usize| match mv {
+        Some(v) => v.inc(i), None => Vec::new().inc(i) };
 
     let lem2idx = Reader::new(BufReader::new(conllx_in)).into_iter()
         .flat_map(|sent| sent.unwrap())
-        .map(|tok| (String::from(tok.lemma().unwrap()),
-                    tok.features().unwrap().as_str().parse::<usize>().unwrap()))
-        .fold(HashMap::new(), |m,(k,x)| update(m, k, grow_posting, x));
-
+        .filter_map(|tok| {
+            match (tok.lemma(), tok.features()) {
+                (Some(lem), Some(feat)) =>
+                    match feat.as_str().parse::<usize>() {
+                        Ok(idx) => Option::Some((String::from(lem),idx)),
+                        Err(_) => { println!("{:?}",tok); Option::None }
+                    },
+                _ => { println!("{:?}",tok); Option::None}
+            }
+        })
+        .fold(HashMap::new(), |m,(k,i)| update(m, k, &|mv| grow_posting(mv,i)));
+    
     let mut wtr = BufWriter::new(index_out);
 
     for (key, val) in lem2idx {
