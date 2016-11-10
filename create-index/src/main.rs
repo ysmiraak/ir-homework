5ec4c9fb14025837e1576b9a7406e74b30dc6e09
Ysmiraak <ysmiraak@gmail.com>
2016-11-10 15:45:12 +0100

    refactoring

--- create-index/src/main.rs
+++ create-index/src/main.rs
@@ -4,10 +4,50 @@ extern crate conllx;
 use protocoll::Map;
 // use std::env::args_os;
 use std::fs::File;
+use conllx::Reader;
 use std::io::{BufReader,BufWriter};
 use std::io::Write;
-use conllx::Reader;
 use std::collections::HashMap;
+
+fn main() {
+    // let mut args = args_os();
+
+    // match (args.nth(1), args.nth(2)) {
+    //     (Some(conllx_in), Some(index_out)) => println!("yes"),
+
+    //     _ => println!("usage: create-index CONLLX_PATH INDEX_PATH"),
+    // }
+
+    let conllx_in_path = "tubadw-r1-ir-sample-1000";
+    let index_out_path = "index.txt";
+
+    let conllx_in = File::open(conllx_in_path).unwrap();
+    let index_out = File::create(index_out_path).unwrap();
+
+    let lem2ids:HashMap<String,Vec<u32>> =
+        Reader::new(BufReader::new(conllx_in)).into_iter()
+        .flat_map(|sent| sent.unwrap())
+        .filter_map(|tok| match (tok.lemma(), tok.features()) {
+            (Some(lem), Some(feat)) =>
+                match feat.as_str().parse::<u32>() {
+                    Ok(idx) => Option::Some((String::from(lem),idx)),
+                    Err(_) => { println!("illformed: {}",tok); Option::None }},
+            _ => { println!("skipping: {}",tok); Option::None }}
+        )
+        .fold(HashMap::new(), |m,(k,i)| Map::update
+              (m, k, |opt_v| SortedSet::inc
+               (if let Some(v) = opt_v {v} else {Vec::new()}, i)));
+
+    let mut wtr = BufWriter::new(index_out);
+
+    for (key,val) in lem2ids {
+        write!(wtr, "{}\t", key).unwrap();
+        for idx in val {
+            write!(wtr, "{} ", idx).unwrap();
+        }
+        writeln!(wtr, "").unwrap();
+    }
+}
 
 pub trait SortedSet<T> where T:Ord+Copy {
     fn inc(self, i:T) -> Self;
@@ -19,44 +59,5 @@ impl<T> SortedSet<T> for Vec<T> where T:Ord+Copy {
             self.insert(idx,i);
         }
         self
-    }
-}
-
-fn main() {
-    // let mut args = args_os();
-
-    // match (args.nth(1), args.nth(2)) {
-    //     (Some(conllx_in), Some(index_out)) => println!("yes"),
-
-    //     _ => println!("usage: create-index CONLLX_PATH INDEX_PATH"),
-    // }
-
-    let conllx_in_file_name = "tubadw-r1-ir-sample-1000";
-    let index_out_file_name = "index.txt";
-
-    let conllx_in = File::open(conllx_in_file_name).unwrap();
-    let index_out = File::create(index_out_file_name).unwrap();
-
-    let lem2idx = Reader::new(BufReader::new(conllx_in)).into_iter()
-        .flat_map(|sent| sent.unwrap())
-        .filter_map(|tok| match (tok.lemma(), tok.features()) {
-            (Some(lem), Some(feat)) =>
-                match feat.as_str().parse::<u32>() {
-                    Ok(idx) => Option::Some((String::from(lem),idx)),
-                    Err(_) => { println!("illformed: {}",tok); Option::None }},
-            _ => { println!("skipping: {}",tok); Option::None }}
-        )
-        .fold(HashMap::new(), |m,(k,i)| Map::update
-              (m, k, |mv:Option<Vec<u32>>| SortedSet::inc
-               (if let Some(v) = mv { v } else { Vec::new() }, i)));
-
-    let mut wtr = BufWriter::new(index_out);
-
-    for (key, val) in lem2idx {
-        write!(wtr, "{}\t", key).unwrap();
-        for idx in val {
-            write!(wtr, "{} ", idx).unwrap();
-        }
-        writeln!(wtr, "").unwrap();
     }
 }
--- query-index/src/main.rs
+++ query-index/src/main.rs
@@ -4,73 +4,16 @@ use std::io::{BufReader,BufRead,stdin};
 use std::collections::HashMap;
 use std::hash::Hash;
 
-pub trait SortedSet<T> {
-    fn intersection(&self, s:&Self) -> Self;
-}
-
-impl<T> SortedSet<T> for Vec<T> where T:Ord+Copy {
-    fn intersection(&self, other:&Self) -> Self {
-        let mut inter = Vec::new();
-
-        let (smaller, larger) = if self.len() < other.len() {
-            (self, other)
-        } else {
-            (other, self)
-        };
-
-        let mut offset = 0;
-        for doc in smaller.into_iter() {
-            offset = match larger[offset..].binary_search(doc) {
-                Ok(idx) => {
-                    inter.push(*doc);
-                    idx
-                }
-                Err(idx) => idx,
-            }
-        }
-        inter
-    }
-}
-
-fn parse_into_map<F,K,V,R>(rdr:R, f:F) -> HashMap<K,V>
-    where F:Fn(String) -> (K,V),
-          K:Hash+Eq,
-          R:BufRead,{
-    rdr.lines()
-        .filter_map(|mline| match mline {
-            Ok(line) => Option::Some(f(line)),
-            Err(_)   => Option::None })
-        .fold(HashMap::new(), |mut m,(k,v)| { m.insert(k,v); m })
-}
-
-fn parse_posting(line:String) -> (String, Vec<u32>) {
-    let x = line.find('\t').unwrap();
-    let term = line[..x].to_string();
-    let list = line[x+1..].split_whitespace()
-        .map(|idx_str| idx_str.parse::<u32>())
-        .filter_map(|midx| match midx {
-            Ok(idx) => Option::Some(idx),
-            Err(_)  => Option::None })
-        .collect();
-    (term, list)
-}
-
-fn parse_idx2doc(line:String) -> (u32, String) {
-    let x = line.find('\t').unwrap();
-    let idx = line[..x].parse::<u32>().unwrap();
-    let doc = line[x+1..].to_string();
-    (idx, doc)
-}
-
 fn main() {
-    let posting_file_name = "index.txt";
-    let idx2doc_file_name = "tubadw-r1-ir-ids-100000.tab";
+    let posting_path = "index.txt";
+    let idx2doc_path = "tubadw-r1-ir-ids-100000.tab";
 
-    let posting_in = File::open(posting_file_name).unwrap();
-    let idx2doc_in = File::open(idx2doc_file_name).unwrap();
+    let posting_in = File::open(posting_path).unwrap();
+    let idx2doc_in = File::open(idx2doc_path).unwrap();
+    
     // try to open both files before parsing either
-    let posting = parse_into_map(BufReader::new(posting_in), parse_posting);
-    let idx2doc = parse_into_map(BufReader::new(idx2doc_in), parse_idx2doc);
+    let posting = parse_to_map(BufReader::new(posting_in), parse_posting);
+    let idx2doc = parse_to_map(BufReader::new(idx2doc_in), parse_idx2doc);
 
     let stdin = stdin();
     println!("enter query:");
@@ -96,5 +39,63 @@ fn main() {
             println!("{}: {}", idx, idx2doc[&idx]);
         }
         println!("\n\nenter query:");
+    }
+}
+
+fn parse_to_map<F,K,V,R>(rdr:R, f:F) -> HashMap<K,V>
+    where F:Fn(String) -> (K,V),
+          K:Hash+Eq,
+          R:BufRead,{
+    rdr.lines()
+        .filter_map(|res_line| match res_line {
+            Ok(line) => Option::Some(f(line)),
+            Err(_)   => Option::None })
+        .fold(HashMap::new(), |mut m,(k,v)| { m.insert(k,v); m })
+}
+
+fn parse_posting(line:String) -> (String, Vec<u32>) {
+    let x = line.find('\t').unwrap();
+    let term = line[..x].to_string();
+    let list = line[x+1..].split_whitespace()
+        .map(|idx_str| idx_str.parse::<u32>())
+        .filter_map(|res_idx| match res_idx {
+            Ok(idx) => Option::Some(idx),
+            Err(_)  => Option::None })
+        .collect();
+    (term, list)
+}
+
+fn parse_idx2doc(line:String) -> (u32, String) {
+    let x = line.find('\t').unwrap();
+    let idx = line[..x].parse::<u32>().unwrap();
+    let doc = line[x+1..].to_string();
+    (idx, doc)
+}
+
+pub trait SortedSet<T> {
+    fn intersection(&self, s:&Self) -> Self;
+}
+
+impl<T> SortedSet<T> for Vec<T> where T:Ord+Copy {
+    fn intersection(&self, other:&Self) -> Self {
+        let mut inter = Vec::new();
+
+        let (smaller, larger) = if self.len() < other.len() {
+            (self, other)
+        } else {
+            (other, self)
+        };
+
+        let mut offset = 0;
+        for doc in smaller.into_iter() {
+            offset = match larger[offset..].binary_search(doc) {
+                Ok(idx) => {
+                    inter.push(*doc);
+                    idx
+                }
+                Err(idx) => idx,
+            }
+        }
+        inter
     }
 }
