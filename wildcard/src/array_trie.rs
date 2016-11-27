5dec996cad156bedeb38f16237c3267503b5d0b3
Ysmiraak <ysmiraak@gmail.com>
2016-11-28 00:41:04 +0100

    cleanup

--- wildcard/src/array_trie.rs
+++ wildcard/src/array_trie.rs
@@ -4,19 +4,22 @@ use trie::Trie;
 
 #[derive(Default,Debug,Clone,PartialEq,Eq,PartialOrd,Ord,Hash)]
 pub struct ArrayMapTrie {
-    pub arrmap:VecSortedMap<char,ArrayMapTrie>,
-    pub accept:bool
+    node:VecSortedMap<char,ArrayMapTrie>,
+    accept:bool
 }
 
 impl ArrayMapTrie {
     pub fn new() -> Self {
-        ArrayMapTrie{arrmap:VecSortedMap::new(),accept:false}
+        ArrayMapTrie {
+            node:VecSortedMap::new(),
+            accept:false
+        }
     }
 
-    fn _prefix_search(&self, prefix:String) -> Vec<String> {
-        self.arrmap.iter().fold
-            (if self.accept {Vec::new().inc(prefix.clone())} else {Vec::new()},
-             |ret,&(c,ref t)| ret.plus(t._prefix_search(prefix.clone().inc(c))))
+    fn search_prefixed(&self, p:String) -> Vec<String> {
+        self.node.iter().fold
+            (if self.accept {Vec::new().inc(p.to_owned())} else {Vec::new()},
+             |ret,&(c,ref t)| ret.plus(t.search_prefixed(p.to_owned().inc(c))))
     }
 }
 
@@ -25,7 +28,7 @@ impl Trie for ArrayMapTrie {
         match s.next() {
             None => {self.accept = true; self}
             Some(c) => ArrayMapTrie {
-                arrmap:self.arrmap.update(c, |opt_t| opt_t.unwrap_or_default().learn(s)).shrink(),
+                node:self.node.update(c, |opt_t| opt_t.unwrap_or_default().learn(s)).shrink(),
                 accept:self.accept
             }
         }
@@ -34,100 +37,22 @@ impl Trie for ArrayMapTrie {
     fn recognize<I>(&self, mut s:I) -> bool where I:Iterator<Item = char> {
         match s.next() {
             None => self.accept,
-            Some(c) => match self.arrmap.get(&c) {
+            Some(c) => match self.node.get(&c) {
                 None => false,
                 Some(t) => t.recognize(s)
             }
         }
     }
 
-    fn prefix_search<I>(&self, mut s:I) -> Vec<String> where I:Iterator<Item = char> {
-        let mut p = String::from("");
+    fn prefix_search<I>(&self, s:I) -> Vec<String> where I:Iterator<Item = char> {
+        let p:String = s.collect();
         let mut t = self;
-        loop {
-            match s.next() {
-                None => return t._prefix_search(p),
-                Some(c) => match t.arrmap.get(&c) {
-                    None => return Vec::new(),
-                    Some(tc) => {p.push(c); t = tc}
-                }
+        for c in p.chars() {
+            t = match t.node.get(&c) {
+                None => return Vec::new(),
+                Some(t) => t
             }
         }
+        t.search_prefixed(p)
     }
 }
-
-
-// #[derive(Debug,Clone,PartialEq,Eq,PartialOrd,Ord,Hash)]
-// pub enum ArrayMapTrie {
-//     Accept(VecSortedMap<char,ArrayMapTrie>),
-//     NonAcc(VecSortedMap<char,ArrayMapTrie>)
-// }
-
-// impl Default for ArrayMapTrie {
-//     fn default() -> Self {
-//         ArrayMapTrie::new()
-//     }
-// }
-
-// impl ArrayMapTrie {
-//     pub fn new() -> Self {
-//         ArrayMapTrie::NonAcc(VecSortedMap::new())
-//     }
-
-//     pub fn is_accept(&self) -> bool {
-//         match self {
-//             &ArrayMapTrie::Accept(_) => true,
-//             &ArrayMapTrie::NonAcc(_) => false
-//         }
-//     }
-
-//     pub fn view_content(&self) -> &VecSortedMap<char,ArrayMapTrie> {
-//         match self {
-//             &ArrayMapTrie::Accept(ref m) => m,
-//             &ArrayMapTrie::NonAcc(ref m) => m
-//         }
-//     }
-
-//     fn _prefix_search(&self, prefix:String) -> Vec<String> {
-//         self.view_content().iter().fold
-//             (if self.is_accept() {Vec::new().inc(prefix.clone())} else {Vec::new()},
-//              |ret,&(c,ref t)| ret.plus(t._prefix_search(prefix.clone().inc(c))))
-//     }
-// }
-
-// impl Trie for ArrayMapTrie {
-//     fn learn<I>(self, mut s:I) -> Self where I:Iterator<Item = char> {
-//         match (self,s.next()) {
-//             (ArrayMapTrie::Accept(m),None) => ArrayMapTrie::Accept(m),
-//             (ArrayMapTrie::NonAcc(m),None) => ArrayMapTrie::Accept(m),
-//             (ArrayMapTrie::Accept(m),Some(c)) => ArrayMapTrie::Accept
-//                 (m.update(c, |opt_t| opt_t.unwrap_or_default().learn(s))),
-//             (ArrayMapTrie::NonAcc(m),Some(c)) => ArrayMapTrie::NonAcc
-//                 (m.update(c, |opt_t| opt_t.unwrap_or_default().learn(s)))
-//         }
-//     }
-
-//     fn recognize<I>(&self, mut s:I) -> bool where I:Iterator<Item = char> {
-//         match s.next() {
-//             None => self.is_accept(),
-//             Some(c) => match self.view_content().get(&c) {
-//                 None => false,
-//                 Some(t) => t.recognize(s)
-//             }
-//         }
-//     }
-
-//     fn prefix_search<I>(&self, mut s:I) -> Vec<String> where I:Iterator<Item = char> {
-//         let mut p = String::from("");
-//         let mut t = self;
-//         loop {
-//             match s.next() {
-//                 None => return t._prefix_search(p),
-//                 Some(c) => match t.view_content().get(&c) {
-//                     None => return Vec::new(),
-//                     Some(tc) => {p.push(c); t = tc}
-//                 }
-//             }
-//         }
-//     }
-// }
--- wildcard/src/btree_trie.rs
+++ wildcard/src/btree_trie.rs
@@ -2,89 +2,57 @@ use std::collections::BTreeMap;
 use protocoll::{Map,Str,Seq};
 use trie::Trie;
 
-#[derive(Debug,Clone,PartialEq,Eq,PartialOrd,Ord,Hash)]
-pub enum BTreeMapTrie {
-    Accept(BTreeMap<char,BTreeMapTrie>),
-    NonAcc(BTreeMap<char,BTreeMapTrie>)
-}
-
-impl Default for BTreeMapTrie {
-    fn default() -> Self {
-        BTreeMapTrie::new()
-    }
+#[derive(Default,Debug,Clone,PartialEq,Eq,PartialOrd,Ord,Hash)]
+pub struct BTreeMapTrie {
+    node:BTreeMap<char,BTreeMapTrie>,
+    accept:bool
 }
 
 impl BTreeMapTrie {
     pub fn new() -> Self {
-        BTreeMapTrie::NonAcc(BTreeMap::new())
-    }
-
-    pub fn is_accept(&self) -> bool {
-        match self {
-            &BTreeMapTrie::Accept(_) => true,
-            &BTreeMapTrie::NonAcc(_) => false
+        BTreeMapTrie {
+            node:BTreeMap::new(),
+            accept:false
         }
     }
 
-    pub fn view_content(&self) -> &BTreeMap<char,BTreeMapTrie> {
-        match self {
-            &BTreeMapTrie::Accept(ref m) => m,
-            &BTreeMapTrie::NonAcc(ref m) => m
-        }
-    }
-
-    fn _learn<I>(self, mut s:I) -> Self where I:Iterator<Item = char> {
-        match (self,s.next()) {
-            (BTreeMapTrie::Accept(m),None) => BTreeMapTrie::Accept(m),
-            (BTreeMapTrie::NonAcc(m),None) => BTreeMapTrie::Accept(m),
-            (BTreeMapTrie::Accept(m),Some(c)) => BTreeMapTrie::Accept
-                (m.update(c, |opt_t| opt_t.unwrap_or_default()._learn(s))),
-            (BTreeMapTrie::NonAcc(m),Some(c)) => BTreeMapTrie::NonAcc
-                (m.update(c, |opt_t| opt_t.unwrap_or_default()._learn(s)))
-        }
-    }
-
-    fn _recognize<I>(&self, mut s:I) -> bool where I:Iterator<Item = char> {
-        match s.next() {
-            None => self.is_accept(),
-            Some(c) => match self.view_content().get(&c) {
-                None => false,
-                Some(t) => t._recognize(s)
-            }
-        }
-    }
-
-    fn _prefix_search(&self, prefix:String) -> Vec<String> {
-        self.view_content().iter()
-            .fold(if self.is_accept() {Vec::new().inc(prefix.clone())} else {Vec::new()},
-                  |ret,(c,t)| ret.plus(t._prefix_search(prefix.clone().inc(*c))))
+    fn search_prefixed(&self, p:String) -> Vec<String> {
+        self.node.iter().fold
+            (if self.accept {Vec::new().inc(p.to_owned())} else {Vec::new()},
+             |ret,(&c,t)| ret.plus(t.search_prefixed(p.to_owned().inc(c))))
     }
 }
 
 impl Trie for BTreeMapTrie {
-    fn learn<I>(self, s:I) -> Self where I:Iterator<Item = char> {
-        self._learn(s)
-    }
-
-    fn recognize<I>(&self, s:I) -> bool where I:Iterator<Item = char> {
-        self._recognize(s)
-    }
-
-    fn prefix_search<I>(&self, mut s:I) -> Vec<String> where I:Iterator<Item = char> {
-        let mut p = String::new();
-        let mut t = self;
-        loop {
-            match s.next() {
-                None => return t._prefix_search(p),
-                Some(c) => {
-                    p.push(c);
-                    match t.view_content().get(&c) {
-                        None => return Vec::new(),
-                        Some(tc) => t = tc
-                    }
-                }
+    fn learn<I>(mut self, mut s:I) -> Self where I:Iterator<Item = char> {
+        match s.next() {
+            None => {self.accept = true; self}
+            Some(c) => BTreeMapTrie {
+                node:self.node.update(c, |opt_t| opt_t.unwrap_or_default().learn(s)),
+                accept:self.accept
             }
         }
     }
-}
 
+    fn recognize<I>(&self, mut s:I) -> bool where I:Iterator<Item = char> {
+        match s.next() {
+            None => self.accept,
+            Some(c) => match self.node.get(&c) {
+                None => false,
+                Some(t) => t.recognize(s)
+            }
+        }
+    }
+
+    fn prefix_search<I>(&self, s:I) -> Vec<String> where I:Iterator<Item = char> {
+        let p:String = s.collect();
+        let mut t = self;
+        for c in p.chars() {
+            t = match t.node.get(&c) {
+                None => return Vec::new(),
+                Some(t) => t
+            }
+        }
+        t.search_prefixed(p)
+    }
+}
