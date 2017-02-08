605f13f2505e2776b72873d13b35e120718a8939
Ysmiraak <ysmiraak@gmail.com>
2017-02-08 19:30:05 +0100

    added kmeans.

---
+++ kmeans/src/bin/kmeans.rs
@@ -0,0 +1,87 @@
+// Author: Kuan Yu, 3913893
+// Honor Code:  I pledge that this program represents my own work.
+
+extern crate kmeans;
+extern crate getopts;
+extern crate rust2vec;
+
+use getopts::Options;
+use std::fs::File;
+use std::path::Path;
+use std::env::args;
+use std::process::exit;
+use std::io::{BufReader, BufWriter, Write};
+use rust2vec::{Embeddings, ReadWord2Vec};
+use kmeans::{kmeans, step_sample, settle_to};
+
+fn main() {
+    let (path_in, path_out, opt_k, epsilon, iter_max, verbose) = {
+        let mut opts = Options::new();
+        opts.reqopt("i", "input", "the binary embeddings file.", "")
+            .optopt("o", "output", "the output tsv file; default: `clusters.tsv`.", "")
+            .optopt("k", "centers", "the number of clusters; default: `sqrt(|data|)`.", "")
+            .optopt("e", "epsilon", "tolerance for convergence; default: `0.05`.", "")
+            .optopt("m", "iter-max", "the maximum number of iterations; default: `25`.", "")
+            .optopt("v", "verbose", "`false` or `true` by default.", "");
+
+        let matches = match opts.parse(args().skip(1)) {
+            Err(e) => {
+                println!("{}", opts.usage(&e.to_string()));
+                exit(1)
+            }
+            Ok(m) => m,
+        };
+
+        (matches.opt_str("i").unwrap(),
+         matches.opt_str("o").unwrap_or("clusters.tsv".to_owned()),
+         matches.opt_str("k").unwrap_or_default().parse::<usize>().ok(),
+         matches.opt_str("e").unwrap_or_default().parse::<f32>().unwrap_or(0.05),
+         matches.opt_str("m").unwrap_or_default().parse::<usize>().unwrap_or(25),
+         matches.opt_str("v").unwrap_or_default().parse::<bool>().unwrap_or(true))
+    };
+
+    if verbose { println!("loading embeddings ...");}
+    let embeddings = {
+        let mut emb = Embeddings::read_word2vec_binary(&mut BufReader::new(open_file(&path_in)))
+            .unwrap();
+        emb.normalize();
+        emb
+    };
+
+    let centroids = {
+        let data = embeddings.data();
+        let k = opt_k.unwrap_or(f32::sqrt(data.rows() as f32) as usize);
+        if verbose { println!("number of clusters:\t{}", k);}
+        kmeans(&data, step_sample(&data, k), epsilon, iter_max, verbose)
+    };
+
+    let mut wtr = BufWriter::new(create_file(&path_out));
+    if verbose { println!("writing to {} ...", path_out);}
+    for (word, embedding) in embeddings.iter() {
+        writeln!(wtr, "{}\t{}", word, settle_to(&embedding, &centroids)).unwrap();
+    }
+}
+
+pub fn open_file<P>(path: P) -> File
+    where P: AsRef<Path>
+{
+    match File::open(&path) {
+        Err(_) => {
+            println!("cannot open file: {:?}", path.as_ref());
+            exit(1)
+        }
+        Ok(file) => file,
+    }
+}
+
+pub fn create_file<P>(path: P) -> File
+    where P: AsRef<Path>
+{
+    match File::create(&path) {
+        Err(_) => {
+            println!("cannot create file: {:?}", path.as_ref());
+            exit(1)
+        }
+        Ok(file) => file,
+    }
+}
---
+++ kmeans/src/lib.rs
@@ -0,0 +1,134 @@
+// Author: Kuan Yu, 3913893
+// Honor Code:  I pledge that this program represents my own work.
+
+extern crate ndarray;
+
+use std::f32;
+use ndarray::{ArrayBase, Array, Array1, Array2, Axis, RemoveAxis, Ix1, Ix2, Data, DataMut};
+
+/// ArrayBase1
+pub type Vector<S> = ArrayBase<S, Ix1>;
+
+/// ArrayBase2
+pub type Matrix<S> = ArrayBase<S, Ix2>;
+
+/// deterministically select `n` samples from `data` along the outer-most axis.
+pub fn step_sample<A, S, D>(data: &ArrayBase<S, D>, n: usize) -> Array<A, D>
+    where A: Copy, S: Data<Elem = A>, D: RemoveAxis
+{
+    let step = data.shape()[0] / n;
+    let mut sel = Vec::new();
+    for i in 0..n {
+        sel.push(i * step);
+    }
+    data.select(Axis(0), &sel)
+}
+
+// /// todo
+// pub fn randome_sample<A, S, D>(data: &ArrayBase<S, D>, n: usize, seed: usize) -> Array<A, D>
+//     where A: Copy, S: Data<Elem = A>, D: RemoveAxis
+
+/// iterativelly recompute the `centroids` til convergence (with `epsilon` error
+/// tolerance), or until `iter_max` is reached. rows in `data` should be
+/// normalized unit vectors.
+pub fn kmeans<S>(data: &Matrix<S>,
+                 mut centroids: Array2<f32>,
+                 epsilon: f32, iter_max: usize, verbose: bool) -> Array2<f32>
+    where S: Data<Elem = f32>
+{
+    for i in 0..iter_max {
+        if verbose { println!("iteration {} ...", i+1);}
+        let new_centroids = {
+            let mut cb = CentroidBuilder::new(centroids.rows(), centroids.cols());
+            for ref v in data.outer_iter() {
+                cb.inc(settle_to(v, &centroids), v);
+            }
+            cb.build(verbose)
+        };
+        if verbose {
+            let (mean, var) = {
+                let mut diff = centroids.outer_iter()
+                    .zip(new_centroids.outer_iter())
+                    .map(|(v1, v2)| distance(&v1, &v2))
+                    .collect::<Array1<f32>>();
+                let mean = diff.scalar_sum() / diff.len() as f32;
+                diff -= mean;
+                (mean, ((&diff * &diff).scalar_sum() / (diff.len() - 1) as f32).sqrt())
+            };
+            println!("shift of centroids:\tmean\t{}\tvar\t{}", mean, var);
+        }
+        if centroids.all_close(&new_centroids, epsilon) {
+            return new_centroids;
+        } else {
+            centroids = new_centroids;
+        }
+    }
+    centroids
+}
+
+/// returns the index of the closest centroid in `centroids` for `data`.
+pub fn settle_to<S1, S2>(data: &Vector<S1>, centroids: &Matrix<S2>) -> usize
+    where S1: Data<Elem = f32>, S2: Data<Elem = f32> {
+    arg_max(&centroids.dot(data))
+}
+
+/// seeing `v: index -> value`, returns the `index` which maximizes `value`, or
+/// `0` for an empty `v`.
+pub fn arg_max<S>(v: &Vector<S>) -> usize where S: Data<Elem = f32> {
+    v.fold((0, 0, f32::NEG_INFINITY),
+           |(m, i, x), y|
+           if x < *y {
+               (i, i+1, *y)
+           } else {
+               (m, i+1, x)
+           }).0
+}
+
+/// scales `v` into a unit vector.
+pub fn normalize<S>(v: &mut Vector<S>) where S: DataMut<Elem = f32> {
+    *v /= v.dot(v).sqrt();
+}
+
+/// returns the euclidean distance between `v1` and `v2`.
+pub fn distance<S1, S2>(v1: &Vector<S1>, v2: &Vector<S2>) -> f32
+    where S1: Data<Elem = f32>,  S2: Data<Elem = f32> {
+    let diff = v1 - v2;
+    diff.dot(&diff).sqrt()
+}
+
+#[derive(Debug,Default,Clone,PartialEq)]
+struct CentroidBuilder {
+    acc: Array2<f32>,
+    cnt: Array1<f32>
+}
+
+impl CentroidBuilder {
+    fn new(k: usize, d: usize) -> Self {
+        CentroidBuilder {
+            acc: Array2::<f32>::zeros((k, d)),
+            cnt: Array1::<f32>::zeros(k)
+        }
+    }
+
+    fn inc<S>(&mut self, i: usize, v: &Vector<S>) where S: Data<Elem = f32> {
+        self.cnt[i] += 1.0;
+        self.acc.row_mut(i) + &v;
+    }
+
+    fn build(mut self, verbose: bool) -> Array2<f32> {
+        for (i, n) in self.cnt.indexed_iter_mut() {
+            if *n == 0.0 {
+                *n = 1.0;
+                if verbose {
+                    println!("cluster {} is uninhabited. its centroid is now the origin.", i)
+                }
+            }
+        }
+        let d = self.cnt.dim();
+        self.acc /= &self.cnt.into_shape((d,1)).unwrap();
+        for ref mut v in self.acc.outer_iter_mut() {
+            normalize(v)
+        }
+        self.acc
+    }
+}
